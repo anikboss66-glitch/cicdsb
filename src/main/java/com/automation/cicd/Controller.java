cd97c09d6340ea253e9910c1b6c2133852c17c4a
Shadman Shariar <shadman.shariar@aspiretss.com>
2026-07-20 20:27:16 +0600

    first commit

---
+++ src/main/java/com/automation/cicd/Controller.java
@@ -0,0 +1,15 @@
+package com.automation.cicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class Controller {
+
+    @GetMapping("/message")
+    public String message (){
+        return "Hello World";
+    }
+}
